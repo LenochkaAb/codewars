a6618c991142b4cc4987eac7ce0fa57de2dea846
lenaab <68326730+lenaab@users.noreply.github.com>
2021-01-15 20:12:27 +0200

    second solution

--- reverse-string.js
+++ reverse-string.js
@@ -1,11 +1,7 @@
 // https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
 
 function solution(str) {
-    let result = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        result = result + str[i];
-    }
-    return result;
+    return str.split('').reverse().join('');
 }
 
 console.log(solution("family"));
